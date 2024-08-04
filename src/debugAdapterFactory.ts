b5718dfb025ebf3c3d90267c0d17fffeefc10e33
Dany Khalife <dev@dkhalife.com>
2024-08-04 10:44:51 -0700

    move debug factory

--- src/debugAdapterFactory.ts
+++ src/debugAdapterFactory.ts
@@ -4,8 +4,16 @@ import * as vscode from 'vscode';
 import { DebugSession } from "./debugSession";
 import { workspaceFileAccessor } from './fileUtils';
 
-export class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
+class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
 	createDebugAdapterDescriptor(_session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
 		return new vscode.DebugAdapterInlineImplementation(new DebugSession(workspaceFileAccessor));
+	}
+}
+
+export function registerDebugAdapterFactory(context: vscode.ExtensionContext) {
+	const factory: vscode.DebugAdapterDescriptorFactory = new InlineDebugAdapterFactory();
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('azurepipelines', factory));
+	if ('dispose' in factory) {
+		context.subscriptions.push(factory as any);
 	}
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,8 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { InlineDebugAdapterFactory } from './debugAdapterFactory';
+import { registerDebugAdapterFactory } from './debugAdapterFactory';
 import { registerConfigurationProviders } from './configurationProvider';
-
-function registerDebugAdapterFactory(context: vscode.ExtensionContext) {
-	const factory: vscode.DebugAdapterDescriptorFactory = new InlineDebugAdapterFactory();
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('azurepipelines', factory));
-	if ('dispose' in factory) {
-		context.subscriptions.push(factory as any);
-	}
-}
 
 export function activate(context: vscode.ExtensionContext) {
 	registerConfigurationProviders(context);
