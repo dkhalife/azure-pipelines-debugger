fd7743bb556b39f62a1538ad837c6b1b284a65d8
Dany Khalife <dev@dkhalife.com>
2024-08-04 15:19:42 -0700

    switch lang to azure-pipelines instead of YAML

--- src/configurationProvider.ts
+++ src/configurationProvider.ts
@@ -8,7 +8,7 @@ class ConfigurationProvider implements vscode.DebugConfigurationProvider {
 		// if launch.json is missing or empty
 		if (!config.type && !config.request && !config.name) {
 			const editor = vscode.window.activeTextEditor;
-			if (editor && editor.document.languageId === 'yaml') {
+			if (editor && editor.document.languageId === 'azure-pipelines') {
 				config.type = 'azurepipelines';
 				config.name = 'Launch';
 				config.request = 'launch';
